2413617c3803aac755aa8fe428fd58a8d9b2752a
Voidmatrix <46210663+VoidmatrixHeathcliff@users.noreply.github.com>
2021-08-11 17:22:42 +0800

    Fixed Timer Module

--- Global.lua
+++ Global.lua
@@ -8,21 +8,24 @@ Global.WINDOWSIZE_H = 900
 Global.GRAVITY = 1
 
 --计时器模块
-local _tempTimer = {delay = 0, callback = nil, params = {}}
 
 local TimerList = {}
 
 function Global.AddTimer(delay, callback, params)
-    _tempTimer.delay, _tempTimer.callback, _tempTimer.params = Time.GetInitTime() + delay, callback, params
-    table.insert(TimerList, _tempTimer)
+    table.insert(TimerList, {
+        delay = Time.GetInitTime() + delay,
+        callback = callback,
+        params = params
+    })
 end
 
 function Global.UpdateTimer()
     local currentTime = Time.GetInitTime()
-    for k, v in ipairs(TimerList) do
-        if v.delay <= currentTime then
-            v.callback(v.params)
-            table.remove(TimerList, k)
+    for i = #TimerList, 1, -1 do
+        local _timer = TimerList[i]
+        if _timer.delay <= currentTime then
+            _timer.callback(_timer.params)
+            table.remove(TimerList, i)
         end
     end
 end
